89acd86846cdde3991c7a97853af215b1e00dcca
gabrielariver <riv23011@byui.edu>
2025-06-17 21:41:50 -0500

    Update vite config profile html page

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         main: 'index.html',
         recipe: 'recipe.html',
         favorites: 'favorites.html',
+        profile: 'profile.html',
       },
     },
   },
